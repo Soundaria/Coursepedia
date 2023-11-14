b5cccfddcb705273bb2d9048b7780e52afa9ed76
Soundaria S <2000107053@hexaware.com>
2023-11-14 14:54:02 +0530

    Initial

---
+++ Course_Api/KampusLearn_6/Migrations/20231105081305_coursedesc.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KampusLearn_6.Migrations
+{
+    /// <inheritdoc />
+    public partial class coursedesc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Course",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageLink",
+                table: "Course",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "ImageLink",
+                table: "Course");
+        }
+    }
+}
